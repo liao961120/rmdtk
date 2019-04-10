3beeec7fcf087d596d058fde02d82fbfc4d6f9ad
liao <liaomovie2@gmail.com>
2019-04-10 21:40:27 +0800

    A function to download fonts only once into the R Markdown template folder
    fix #2 & #4

--- R/download_font.R
+++ R/download_font.R
@@ -1,11 +1,25 @@
 #' Download font dependencies for traditional Chinese Rmd PDF output
 #'
+#' This function is expected to run once after the package is
+#' downloaded. It downloads font files into the \code{rmdtk}'s
+#' R Markdown template folders, so when a template is used
+#' later, it will contained the necessary fonts to work.
+#'
+#' @export
+download_fonts <- function() {
+  font_dest_dir <- system.file('rmarkdown', 'templates', 'CJKdocument', 'skeleton', 'latex', package = 'rmdtk')
+
+  download_file2(font_dest_dir)
+}
+
+#' Download font dependencies to the directory where header.tex is
+#'
 #' Make sure this function is called after 1) importing Rmd template
 #' and 2) setting the current working directory to that Rmd
 #' template folder.
 #'
-#' @export
-donwload_font <- function() {
+#' @keywords internal
+donwload_font2header <- function() {
   header_tex_path <- list.files(getwd(), 'header.tex', recursive = T, full.names = T)
 
   # Check header.tex path
@@ -43,4 +57,22 @@ download_file <- function(ori_dir) {
   # Decompress
   utils::unzip(temp_kai, exdir = cwd)
   utils::unzip(temp_mono, exdir = cwd)
+}
+
+
+
+download_file2 <- function(dest_dir) {
+
+  kaiti <- 'https://github.com/sih4sing5hong5/huan1-ik8_lun7-bun5/raw/master/%E5%AD%97%E9%AB%94/kaiu.ttf'
+  mono <- 'https://liao961120.github.io/deps/fonts/NotoSansMonoCJKtc.zip'
+
+  cat('Downloading fonts to', dest_dir, '\n')
+  temp_kai <- tempfile()
+  temp_mono <- tempfile()
+
+  utils::download.file(kaiti, destfile = file.path(dest_dir, 'kaiti.ttf'))
+  utils::download.file(mono, destfile = temp_mono)
+
+  # Decompress
+  utils::unzip(temp_mono, exdir = dest_dir)
 }
